77f0de81e644c700e0cec17d2adb45f4efd99d46
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-06-01 12:21:56 -0500

    completed Advanced Array Methods in object-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -96,6 +96,7 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
+zooAnimals.forEach(animal => animalNames.push(`Name: ${animal.animal_name}, Scientific: ${animal.scientific_name}`));
 console.log(animalNames);
 
 /* Request 2: .map()    
@@ -103,8 +104,7 @@ console.log(animalNames);
 The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
 
 */
-
-const lowerCase = [];
+const lowerCase = zooAnimals.map(animal => animal.animal_name.toLowerCase());
 console.log(lowerCase); 
 
 /* Request 3: .filter() 
@@ -112,7 +112,7 @@ console.log(lowerCase);
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const lowerPopulation = [];
+const lowerPopulation = zooAnimals.filter(animal => animal.population < 5);
 console.log(lowerPopulation);
 
 /* Request 4: .reduce() 
@@ -120,7 +120,7 @@ console.log(lowerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce((previousVal, currentVal) => currentVal.population + previousVal, 0);
 console.log(populationTotal);
 
 
@@ -129,4 +129,3 @@ console.log(populationTotal);
 Stretch: If you haven't already, convert your array method callbacks into arrow functions.
 
 */
-
